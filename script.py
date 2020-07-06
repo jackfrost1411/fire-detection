95114d3b79c07cd1cb437362f7298f5100e6cdbc
Dhruvil Shah <48760936+jackfrost1411@users.noreply.github.com>
2020-07-06 11:59:38 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,31 +4,24 @@ from PIL import Image
 import tensorflow as tf
 from keras.preprocessing import image
 #Load the saved model
-model = tf.keras.models.load_model('Inception22.h5')
+model = tf.keras.models.load_model('InceptionV3.h5')
 video = cv2.VideoCapture(0)
-
 while True:
         _, frame = video.read()
-
-        #Convert the captured frame into RGB
+#Convert the captured frame into RGB
         im = Image.fromarray(frame, 'RGB')
-
-        #Resizing into 128x128 because we trained the model with this image size.
+#Resizing into 224x224 because we trained the model with this image size.
         im = im.resize((224,224))
         img_array = image.img_to_array(im)
-
-        #Our keras model used a 4D tensor, (images x height x width x channel)
-        #So changing dimension 128x128x3 into 1x128x128x3 
         img_array = np.expand_dims(img_array, axis=0) / 255
         probabilities = model.predict(img_array)[0]
-        #Calling the predict method on model to predict 'me' on the image
+        #Calling the predict method on model to predict 'fire' on the image
         prediction = np.argmax(probabilities)
-        #if prediction is 0, which means I am missing on the image, then show the frame in gray color.
+        #if prediction is 0, which means there is fire in the frame.
         if prediction == 0:
                 frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
                 print(probabilities[prediction])
-
-        cv2.imshow("Capturing", frame)
+cv2.imshow("Capturing", frame)
         key=cv2.waitKey(1)
         if key == ord('q'):
                 break
